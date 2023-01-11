9f634271daf7ac9210a7e54168dfa3582a9e53c9
Rawan <roalsufyani22@twq.idserve.net>
2023-01-11 11:03:32 +0300

    Initial Commit

---
+++ Sources/SourcesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourcesApp.swift
+//  Sources
+//
+//  Created by Rawan on 18/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SourcesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
